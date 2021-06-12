5cecd865718a854cb4bd8dbd3fcc9e60380c299d
unknown <Phasakornphooweang@gmail.com>
2021-06-12 17:27:07 +0700

    Edit USB CAN Project

--- usbCAN/Core/Inc/stm32f4xx_it.h
+++ usbCAN/Core/Inc/stm32f4xx_it.h
@@ -59,7 +59,6 @@ void SysTick_Handler(void);
 void EXTI2_IRQHandler(void);
 void CAN1_TX_IRQHandler(void);
 void CAN1_RX0_IRQHandler(void);
-void EXTI15_10_IRQHandler(void);
 void OTG_FS_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
--- usbCAN/USB_DEVICE/App/usbd_cdc_if.c
+++ usbCAN/USB_DEVICE/App/usbd_cdc_if.c
@@ -262,13 +262,13 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
   /* USER CODE BEGIN 6 */
-		USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
-		USBD_CDC_ReceivePacket(&hUsbDeviceFS);
-		USBD_CDC_HandleTypeDef* hcdc = (USBD_CDC_HandleTypeDef*)hUsbDeviceFS.pClassData;
-		extern int usbRxDataSize;
-		usbRxDataSize = hcdc->RxLength;
-		UserRxBufferFS[usbRxDataSize] = 0; 
-		return (USBD_OK);
+  USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
+  USBD_CDC_ReceivePacket(&hUsbDeviceFS);
+	USBD_CDC_HandleTypeDef* hcdc = (USBD_CDC_HandleTypeDef*)hUsbDeviceFS.pClassData;
+	extern int usbRxDataSize;
+	usbRxDataSize = hcdc->RxLength;
+	UserRxBufferFS[usbRxDataSize] = 0;
+  return (USBD_OK);
   /* USER CODE END 6 */
 }
 
